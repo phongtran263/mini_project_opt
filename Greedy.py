e982752870384705b450770821ded4613287be32
khanhnn20042002 <92853125+khanhnn20042002@users.noreply.github.com>
2021-12-25 20:04:56 +0700

    Update Greedy.py

--- Greedy.py
+++ Greedy.py
@@ -1,7 +1,7 @@
-from random_generate import gen
+from mini_project_instance_generator import Gen
 import time
-
-def input(filename):
+import random as rd
+def Input(filename):
 	t = []
 	g = []
 	s = []
@@ -13,7 +13,7 @@ def input(filename):
 			g.append(l[1])
 			s.append(l[2])
 		c = [int(x) for x in f.readline().split()]
-	return N, M, t, g, s, c
+		return N, M, t, g, s, c
 
 def Time(i):
     if 1<= i <= 12:
@@ -27,21 +27,17 @@ def Time(i):
     else:
         day = 'Friday'
     j = i % 12
-    if 1 <= j <= 6:
-        daytime = 'Morning'
-        lesson = j
+    if j == 0:
+        lesson = 12
     else:
-        daytime = 'Afternoon'
-        if j == 0:
-            lesson = 6
-        else:
-            lesson = j - 6
-    return lesson,daytime,day
+        lesson = j
+    return lesson,day
 
 def PrintSolution(solution):
     if solution == 'Failure':
         print(solution)
         return
+    print('Solution is found')
     for k in range(len(N)):
         for i in range(t[k]):
             Class = k + 1
@@ -49,8 +45,7 @@ def PrintSolution(solution):
             room += 1
             time = Time(time+1)
             print(f'Class {Class}' + ' '*(3 - len(str(Class))) + f'has a lesson in room {room}'
-                  + ' '*(3 - len(str(room)))+ f'at the {time[0]}' + f' lesson in the {time[1]}' 
-                  + ' ' * (9 - len(str(time[1]))) + f' on {time[2]}')
+                  + ' '*(3 - len(str(room)))+ f'at the {time[0]}' + f' shift on {time[1]}' )
         print()    
 
 def Select(solution,var):
@@ -62,8 +57,8 @@ def Select(solution,var):
                 if Feasible(new_solution):
                     return (i,j)
     return False
-
-
+    
+    
 def Feasible(solution):
     d1 = {}
     d2 = {}
@@ -71,14 +66,14 @@ def Feasible(solution):
         Class = i[0]
         time_slot = j[0]
         room = j[1]
-        # capacity of the room must be less than the number of students in a class
+        # capacity of the room must be less than the number of students of a class
         if c[room] < s[Class]:
             return False
         # at a given time, a class can't take part in more than 1 lecture
         d1[Class,time_slot] = d1.get((Class,time_slot),-1) + 1
         if d1[Class,time_slot] > 0:
             return False
-        # two class having the same teacher can't study at the same time 
+        # two class having the same teacher can't study at the same time
         d2[time_slot] = d2.get(time_slot,set())
         d2[time_slot].add(Class)
        
@@ -103,30 +98,30 @@ def Greedy():
     return solution
 
 if __name__ == '__main__':
-    filename = "random_data.txt"
-    gen(filename, 15, 4, hard=False)
-    N,M,t,g,s,c = input(filename)
+    N,M,t,g,s,c = Input('test.txt')
     num_time_slots = 5*12
     candidates =  {}
     for i in range(num_time_slots):
         for j in range(M):
             candidates[i,j] = True
-    # sort the room set in increasing order with respect to its capacity
-    '''M = [(c[i],i) for i in range(M)]
+    # sort the room set in increasing order in terms of its capacity
+    M = [(c[i],i) for i in range(M)]
     M.sort()
     M = [i[1] for i in M]
-    # sort the class set in decreasing order with respect to its number of students
+    # sort the class set in decreasing order in terms of its number of students
     N = [(s[i],i) for i in range(N)]
     N.sort(reverse = True)
-    N = [i[1] for i in N]'''
-    M = range(M)
-    N = range(N)
+    N = [i[1] for i in N]
+    '''M = range(M)
+    N = range(N)'''
     start = time.time()
     solution = Greedy()
     end = time.time()
     PrintSolution(solution)
     print('Time:',end - start)
 
+            
+                
             
                 
     
