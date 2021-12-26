1d9b735b8b86e6fceec6aac4a56b3fb428aab395
huydsai02 <84078549+huydsai02@users.noreply.github.com>
2021-12-26 11:26:08 +0700

    .

--- AnotherGreedy.py
+++ AnotherGreedy.py
@@ -2,52 +2,27 @@ from random_generate import gen
 import time
 
 def input(filename):
-	t = []
-	g = []
-	s = []
-	with open(filename) as f:
-		[N, M] = [int(x) for x in f.readline().split()]
-		for _ in range(N):
-			l = [int(x) for x in f.readline().split()]
-			t.append(l[0])
-			g.append(l[1])
-			s.append(l[2])
-		c = [int(x) for x in f.readline().split()]
-	return N, M, t, g, s, c
+  with open(filename) as f:
+    [N, M] = [int(x) for x in f.readline().split()]
+    info_classes = [[int(x) for x in f.readline().split()] for _ in range(N)]
+    rooms = [int(x) for x in f.readline().split()]
+  return info_classes, rooms
 
-def Solve(filename):
-  N, M, t, g, s, c = input(filename)
-  info_classes = []
-  sum_period = 0
-  teacher = []
-  for i in range(N):
-    info_classes.append([t[i],g[i],s[i]])
-    sum_period += t[i]
-    teacher.append(g[i])
+def Greedy(filename):
+  info_classes, rooms = input(filename)
+  teacher = [g for t, g, s in info_classes]
   state_teacher = [[True for _ in range(len(set(teacher)) + 1)] for __ in range(60)]
-  state_room = [[True for _ in range(M)] for __ in range(60)]
-  Greedy(info_classes, state_room, state_teacher, c)
-
-def Greedy(info_classes, state_room, state_teacher, rooms):
+  state_room = [[True for _ in range(len(rooms))] for __ in range(60)]
   timetable = {}
   for i in range(len(info_classes)):
     info_class = info_classes[i]
     timetable[i] = []
     for _ in range(info_class[0]):
-      found = False
-      for p in range(60):
-        for r in range(len(rooms)):
-          if Feasible(info_class, state_room, state_teacher, rooms, p, r):
-            state_room[p][r] = False
-            state_teacher[p][info_class[1]] = False
-            timetable[i].append((p, r))
-            found = True
-            break
-        if found:
-          break
-      if not found:
+      x = Select(info_class, state_room, state_teacher, rooms)
+      if x == None:
         print("FAILURE")
         return False
+      timetable[i].append(x)
   PrintSolution(timetable)
   return True
 
@@ -63,6 +38,15 @@ def Feasible(info_class, state_room, state_teacher, rooms, p, r):
     return False
   return True
 
+def Select(info_class, state_room, state_teacher, rooms):
+  for p in range(60):
+    for r in range(len(rooms)):
+      if Feasible(info_class, state_room, state_teacher, rooms, p, r):
+        state_room[p][r] = False
+        state_teacher[p][info_class[1]] = False
+        return p, r
+  return None
+
 def PrintSolution(timetable):
   classes = sorted(list(timetable.keys()))
   convert = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
@@ -76,6 +60,6 @@ if __name__ == "__main__":
   filename = "random_data.txt"
   gen(filename, 15, 2, hard=True)
   start = time.time()
-  Solve(filename)
+  Greedy(filename)
   end = time.time()
   print("Time:", end - start)
