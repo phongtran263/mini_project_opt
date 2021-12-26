60b1c9efcec14d945b9498a98fe329fa4f2ed297
huydsai02 <84078549+huydsai02@users.noreply.github.com>
2021-12-26 13:11:30 +0700

    .

--- AnotherGreedy.py
+++ AnotherGreedy.py
@@ -4,45 +4,52 @@ import time
 def input(filename):
   with open(filename) as f:
     [N, M] = [int(x) for x in f.readline().split()]
-    info_classes = [[int(x) for x in f.readline().split()] for _ in range(N)]
+    info_classes = [[int(x) for x in f.readline().split()] + [_ + 1] for _ in range(N)]
     rooms = [int(x) for x in f.readline().split()]
   return info_classes, rooms
 
 def Greedy(filename):
   info_classes, rooms = input(filename)
-  teacher = [g for t, g, s in info_classes]
+  # Sort depend on room's capacity (increasing) 
+  rooms_sort = sorted([(rooms[i], i) for i in range(len(rooms))], key = lambda x : x[0])
+  
+  # Sort depend on the number of student (decreasing)
+  info_classes_sort = sorted(info_classes, key = lambda x: -x[2])
+  teacher = [g for t, g, s, _ in info_classes]
   state_teacher = [[True for _ in range(len(set(teacher)) + 1)] for __ in range(60)]
   state_room = [[True for _ in range(len(rooms))] for __ in range(60)]
   timetable = {}
-  for i in range(len(info_classes)):
-    info_class = info_classes[i]
-    timetable[i] = []
+  for i in range(len(info_classes_sort)):
+    info_class = info_classes_sort[i]
+    timetable[info_class[-1]] = []
     for _ in range(info_class[0]):
-      x = Select(info_class, state_room, state_teacher, rooms)
+      x = Select(info_class, state_room, state_teacher, rooms_sort)
       if x == None:
         return "FAILURE"
-      timetable[i].append(x)
+      timetable[info_class[-1]].append(x)
   return timetable
 
-def Feasible(info_class, state_room, state_teacher, rooms, p, r):
+def Feasible(info_class, state_room, state_teacher, info_room, p):
+  capacity, index_room = info_room
   # At a moment, a teacher teaches at most one class
   if not state_teacher[p][info_class[1]]:
     return False
   # At a moment, a room is used by at most one class
-  if not state_room[p][r]:
+  if not state_room[p][index_room]:
     return False
   # The room's capacity is bigger than the number of student in class
-  if info_class[2] > rooms[r]:
+  if info_class[2] > capacity:
     return False
   return True
 
 def Select(info_class, state_room, state_teacher, rooms):
   for p in range(60):
     for r in range(len(rooms)):
-      if Feasible(info_class, state_room, state_teacher, rooms, p, r):
-        state_room[p][r] = False
+      if Feasible(info_class, state_room, state_teacher, rooms[r], p):
+        _, index_room = rooms[r]
+        state_room[p][index_room] = False
         state_teacher[p][info_class[1]] = False
-        return p, r
+        return p, index_room
   return None
 
 def PrintSolution(timetable):
@@ -54,7 +61,7 @@ def PrintSolution(timetable):
   shift = lambda x: 12 if x % 12 == 0 else x % 12
   for i in classes:
     for p, r in timetable[i]:
-      print(f'Class {i+1} has lessons at period {shift(p+1)} on {convert[p//12]} at room {r}')
+      print(f'Class {i} has lessons at period {shift(p+1)} on {convert[p//12]} at room {r}')
     print()
 
 if __name__ == "__main__":
